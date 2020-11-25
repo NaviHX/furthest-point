ec20e8d61b2161610e2c0b6f9e774c47d5529034
NaviHX <932942142@qq.com>
2020-11-25 15:23:46 +0800

    main函数流程

--- main.cpp
+++ main.cpp
@@ -24,3 +24,29 @@ vertex ve[1001];
 
 void addEdge(int from,int to,int weight);
 void vertexInit(vertex* v);
+
+int main()
+{
+    int arg;
+    do
+    {
+        cout << "请输入功能选项\n1.求最远两点\n0.结束程序\n选项:";
+        cin >> arg;
+        switch (arg)
+        {
+        case 1:
+            /* code */
+            break;
+        case 0:
+            cout << "感谢使用\n";
+            break;
+        default:
+            cout << "无效的选项\n";
+            break;
+        }
+        cout << "\n";
+    } while (arg);
+    return 0;
+}
+
+
